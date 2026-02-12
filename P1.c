843bdcb070b27223b10f03320e403b3245360a46
dipika290 <dipeeka560@gmail.com>
2026-02-12 22:28:46 +0530

    arithmetic.c

---
+++ P1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+  printf ("\n arithmetic");
+int a=10;
+int b=20;
+printf ("enter value of a and b");
+printf (" addition=%d",a+b);
+printf ("sub=%d",a-b);
+printf ("multi=%d",a*b);
+printf ("div=%d",a/b);
+printf ( "module=%d",a%b);
+return 0;
+}
